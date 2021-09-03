c94a9f737ebffc0d043e6ffcdc31919efed7f501
ndy0 <ndy0@debian.localhost>
2021-09-03 10:56:41 +0200

    v0.1.15

--- src/blob-read-stream.ts
+++ src/blob-read-stream.ts
@@ -8,13 +8,17 @@ class BlobReadStream extends Readable {
         this.size = blob.size;
     }
     _read(size: number): void {
+        let isLast = false;
         const newOffset = this.offset + size > this.size ? this.size : this.offset + size;
         const chunk = this.blob.slice(this.offset, newOffset);
+        this.offset = newOffset;
+        if(this.size === this.offset) {
+            isLast = true;
+        }
         chunk.arrayBuffer().then((data: ArrayBuffer) => {
-            this.push(Buffer.from(data))
-            this.offset = newOffset;
-            if(this.offset === this.size) {
-                this.push(null)
+            this.push(Buffer.from(data));
+            if(isLast) {
+                this.push(null);
             }
         })
     }
